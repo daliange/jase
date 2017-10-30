614819f3ef648c9d0a664496560d6cfca6db58b4
li.jie <li.jie@sand.com.cn>
2017-10-30 15:01:40 +0800

    基础类型保存与比较

--- src/main/java/com/lijie/java/jase/Primitive.java
+++ src/main/java/com/lijie/java/jase/Primitive.java
@@ -32,7 +32,7 @@ public class Primitive
     	short maxs = 32767;
     	
     	/**
-    	 * java 的基本数据类型
+    	 * java 的基本数据类型，保存在栈里
     	 * byte 	8bits
     	 * short 	16bits
     	 * int 		32bits
@@ -45,6 +45,60 @@ public class Primitive
     	 * boolean	8bits
     	 * **/
     	
+    	/**
+    	 * java引用类型，对象本身保存在堆上，引用保存在栈上
+    	 * 
+    	 * **/
+    	
+    	
+    	/**
+    	 * 变量的比较
+    	 * == 用来比较基本类型
+    	 * == 比较其他类型，指的是否引用
+    	 * 
+    	 * **/
+    	
+    	int a = 1;
+    	int b = 1;
+    	if(a==b){
+    		System.out.println("int a==b,"+"true");
+    	}
+    	
+    	
+    	/**
+    	 * c,d保存在栈上
+    	 * "abc"保存在常量区，编译器优化后，存在一个常量不会新建
+    	 * 
+    	 * 
+    	 * **/
+    	String c = "abc";
+    	String d = "abc";
+    	if(c==d){
+    		System.out.println("String c==d,"+"true");
+    	}else{
+    		System.out.println("String c==d,"+"false");
+    	}
+    	
+    	//存放在虚拟机常量池
+    	String g = "abc";
+    	//存放在堆上
+    	String h = new String("abc");
+    	if(g==h){
+    		System.out.println("String g==h,"+"true");
+    	}else{
+    		System.out.println("String g==h,"+"false");
+    	}
+    	
+    	
+    	
+    	
+    	Function e = new Function();
+    	Function f = new Function();
+    	if(e==f){
+    		System.out.println("Class e==f,"+"true");
+    	}else{
+    		System.out.println("Class e==f,"+"false");
+    	}
     	
     }
 }
