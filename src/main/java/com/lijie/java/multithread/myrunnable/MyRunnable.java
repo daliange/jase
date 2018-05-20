2a69ecef60c934f922c73f7f11900c0936606fb9
li.jie <li.jie@sand.com.cn>
2018-05-20 16:30:35 +0800

    给MyThread设置名称

--- src/main/java/com/lijie/java/multithread/myrunnable/MyRunnable.java
+++ src/main/java/com/lijie/java/multithread/myrunnable/MyRunnable.java
@@ -2,9 +2,9 @@ package com.lijie.java.multithread.myrunnable;
 
 public class MyRunnable implements Runnable {
 
+	/**实现Runnable接口**/
 	public void run() {
 		System.out.println("MyRunnable="+Thread.currentThread().getName());
-
 	}
 
 }
--- src/main/java/com/lijie/java/multithread/mythread/Test.java
+++ src/main/java/com/lijie/java/multithread/mythread/Test.java
@@ -4,8 +4,13 @@ public class Test {
 	
 	public static void main(String[] args) {
 		MyThread myThread = new MyThread();
+		/**给MyThread设置名称**/
+		myThread.setName("my name is MyThread ! ");
 		myThread.start();
-		System.out.println("运行结束！");
+		Thread.currentThread().setName("my name is main ! ");
+		System.out.println("主线程名称："+Thread.currentThread().getName());
+		System.out.println("主线程：运行结束！");
+		
 	}
 
 }
