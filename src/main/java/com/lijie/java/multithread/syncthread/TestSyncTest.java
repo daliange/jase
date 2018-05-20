cf990a99d4a3fc5650bfe563990a49b15e774ab2
li.jie <li.jie@sand.com.cn>
2018-05-20 17:39:58 +0800

    关键字synchronized很重要

---
+++ src/main/java/com/lijie/java/multithread/syncthread/TestSyncTest.java
@@ -0,0 +1,48 @@
+package com.lijie.java.multithread.syncthread;
+
+public class TestSyncTest {
+	
+	public static void main(String[] args) {
+		SyncThread syncThread = new SyncThread();
+		Thread a = new Thread(syncThread);
+		a.setName("Thread a :");
+		Thread b = new Thread(syncThread);
+		b.setName("Thread b :");
+		a.start();
+		b.start();
+	}
+}
+
+class SyncThread implements Runnable{
+	
+	private int balance=0;
+	
+	public void run() {
+		for(int i= 0 ;i<50; i++){
+			System.out.println(Thread.currentThread().getName()+"更新前"+balance);
+			try {
+				incrementnew();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName()+"更新后"+balance);
+		}
+	}
+	
+	/**关键字synchronized很重要**/
+	public synchronized void incrementold() throws InterruptedException{
+		int i = balance;
+		Thread.currentThread().sleep(10);
+		balance = i+1;
+	}
+	
+	public void incrementnew() throws InterruptedException{
+		synchronized(this){
+			int i = balance;
+			Thread.currentThread().sleep(10);
+			balance = i+1;
+		}
+	}
+	
+}
